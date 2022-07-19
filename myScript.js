335186525f38716bf4c042d1ac4820325f76a223
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-07-18 19:22:50 -0500

    Fix float menu

--- myScript.js
+++ myScript.js
@@ -1,3 +1,19 @@
-const revisar = document.querySelector('#button');
+const modal = document.querySelector('#modal-menu');
+const hambIcon = document.querySelector('#burguer-icon');
+const xIcon = document.querySelector('#x-icon');
+const list = document.querySelector ('#list-menu');
 
-console.log(revisar);
+
+hambIcon.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+xIcon.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+list.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+console.log(modal);
