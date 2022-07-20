6cbd7a5a5b16a9f19ce31726de49998473a2baa0
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-07-19 23:03:44 -0500

    End mobil version with JavaScript

--- myScript.js
+++ myScript.js
@@ -19,12 +19,12 @@ list.addEventListener('click', () => {
 // function to open and close the popup windows
 // project# =  ['Titulo'        , 'Description'          , 'img1', 'img', 'img', 'img', 'img','tech1','tech2','tech3','link live version','link to source']
 let project1 = ['First project',
-                 'Welcome to my project',
+                 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, rem!',
                  '/assets/hd1.png',
-                 '/assets/desktop/desk-img-1.png',
-                 '/assets/desktop/desk-img-2.png',
-                 '/assets/desktop/desk-img-3.png',
-                 '/assets/desktop/desk-img-4.png',
+                 '/assets/Rectangle21(1).png',
+                 '/assets/Rectangle21(2).png',
+                 '/assets/Rectangle21(3).png',
+                 '/assets/Rectangle21(4).png',
                  'HTML',
                  'Ruby on Rails',
                  'JavaScript',
@@ -32,12 +32,64 @@ let project1 = ['First project',
                  'https://github.com/ErikStoupignan/My_Portfolio'];
 
 let project2 = ['Second project',
-                 'Welcome to my project',
-                 '/assets/desktop/desk-img-4.png',
+                 'Lorem, ipsum dolor sit amet consectetur adipisicing.',
+                 '/assets/Rectangle21(4).png',
                  '/assets/hd1.png',
-                 '/assets/desktop/desk-img-1.png',
-                 '/assets/desktop/desk-img-2.png',
-                 '/assets/desktop/desk-img-3.png',
+                 '/assets/Rectangle21(1).png',
+                 '/assets/Rectangle21(2).png',
+                 '/assets/Rectangle21(3).png',
+                 'html',
+                 'Ruby on Rails',
+                 'JavaScript',
+                 'https://github.com/ErikStoupignan/My_Portfolio',
+                 'https://github.com/ErikStoupignan/My_Portfolio'];
+
+let project3 = ['Super project',
+                 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed cumque qui tenetur ducimus architecto!',
+                 '/assets/Rectangle21(3).png',
+                 '/assets/Rectangle21(4).png',
+                 '/assets/hd1.png',
+                 '/assets/Rectangle21(1).png',
+                 '/assets/Rectangle21(2).png',
+                 'html',
+                 'Ruby on Rails',
+                 'JavaScript',
+                 'https://github.com/ErikStoupignan/My_Portfolio',
+                 'https://github.com/ErikStoupignan/My_Portfolio'];
+
+let project4 = ['Arduino developer',
+                 'Adipisicing elit. Sed cumque qui tenetur ducimus architecto!',
+                 '/assets/Rectangle21(2).png',
+                 '/assets/Rectangle21(3).png',
+                 '/assets/Rectangle21(4).png',
+                 '/assets/hd1.png',
+                 '/assets/Rectangle21(1).png',
+                 'html',
+                 'Ruby on Rails',
+                 'JavaScript',
+                 'https://github.com/ErikStoupignan/My_Portfolio',
+                 'https://github.com/ErikStoupignan/My_Portfolio'];
+
+let project5 = ['Pressure Sensor',
+                 'Ipsum dolor sit amet, consectetur adipisicing elit. Nulla, iusto eum.Adipisicing elit. Sed cumque qui tenetur ducimus architecto!',
+                 '/assets/Rectangle21(1).png',
+                 '/assets/Rectangle21(2).png',
+                 '/assets/Rectangle21(3).png',
+                 '/assets/Rectangle21(4).png',
+                 '/assets/hd1.png',
+                 'html',
+                 'Ruby on Rails',
+                 'JavaScript',
+                 'https://github.com/ErikStoupignan/My_Portfolio',
+                 'https://github.com/ErikStoupignan/My_Portfolio'];
+
+let project6 = ['Microverse Programm!',
+                 'Consectetur adipisicing elit. Nulla, iusto eum.Adipisicing elit. Sed cumque qui tenetur ducimus architecto!',
+                 '/assets/Microverse-photo.jpg',
+                 '/assets/Rectangle21(2).png',
+                 '/assets/Rectangle21(3).png',
+                 '/assets/Rectangle21(4).png',
+                 '/assets/hd1.png',
                  'html',
                  'Ruby on Rails',
                  'JavaScript',
@@ -48,48 +100,55 @@ let project2 = ['Second project',
 // function to put the information inside the Popup 
 function popUp(string) {
 
+  let box;
    // Project1
   if ( string == 'project1'){
-  document.getElementById("popup-1").classList.toggle("active");
-
-  document.getElementById("Popup-title").innerHTML = project1[0];
-  document.getElementById("Popup-text").innerHTML = project1[1];
-  document.getElementById("popup-img1").scr = project1[2];
-  document.getElementById("popup-img2").scr = project1[3];
-  document.getElementById("popup-img3").scr = project1[4];
-  document.getElementById("popup-img4").scr = project1[5];
-  document.getElementById("popup-img5").scr = project1[6];
-  document.getElementById("pop-tech-1").innerHTML = project1[7];
-  document.getElementById("pop-tech-2").innerHTML = project1[8];
-  document.getElementById("pop-tech-3").innerHTML = project1[9];
-  document.getElementById("pop-link-1").href = project1[10];
-  document.getElementById("pop-link-2").href = project1[11];
+    box = project1;
   } 
 
     // Project2
   if ( string == 'project2'){
-    console.log(string);
+    box = project2;
   } 
 
     // Project3
   if ( string == 'project3'){
-    console.log(string);
+    box = project3;
   } 
 
     // Project4
   if ( string == 'project4'){
-    console.log(string);
+    box = project4;
   } 
 
     // Project5
    if ( string == 'project5'){
-    console.log(string);
+    box = project5;
   } 
 
     // Project6
   if ( string == 'project6'){
-    console.log(string);
+    box = project6;
   } 
+
+  if ( box != undefined ){
+    document.getElementById("popup-1").classList.toggle("active");
+    document.getElementById("Popup-title").innerHTML = box[0];
+    document.getElementById("Popup-text").innerHTML = box[1];
+    document.getElementById("popup-img1").src = box[2];
+    document.getElementById("popup-img2").src = box[3];
+    document.getElementById("popup-img3").src = box[4];
+    document.getElementById("popup-img4").src = box[5];
+    document.getElementById("popup-img5").src = box[6];
+    document.getElementById("pop-tech-1").innerHTML = box[7];
+    document.getElementById("pop-tech-2").innerHTML = box[8];
+    document.getElementById("pop-tech-3").innerHTML = box[9];
+    document.getElementById("pop-link-1").href = box[10];
+    document.getElementById("pop-link-2").href = box[11];
+  }
+
+
+
 }
 
 // function to exit 
