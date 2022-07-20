8954399fce5e3861b53a963764c6ea50c8b7b50a
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-07-20 11:37:31 -0500

    Fix linebreaks on JavaScript file CRLF for LF

--- myScript.js
+++ myScript.js
@@ -1,5 +1,3 @@
-/* eslint linebreak-style: ["error", "windows"] */
-
 // Hamburguer button works
 const modal = document.querySelector('#modal-menu');
 const hambIcon = document.querySelector('#burguer-icon');
