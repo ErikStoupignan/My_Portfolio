e3183b17d7ff343ea61a8037dc5ec2a13f9a5ec3
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2023-02-21 10:18:30 -0600

    Active form to recived emails

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
-// className="inputs-group"
-// action="https://formspree.io/f/mrgjwpka"
-// method="POST"
-// encType="multipart/form-data"
-
 const Form = () => (
-  <form>
+  <form
+    className="inputs-group"
+    action="https://formspree.io/f/mrgjwpka"
+    method="POST"
+    encType="multipart/form-data"
+  >
     <label htmlFor="name">
       Your name
       <input type="text" id="name" name="name" />
